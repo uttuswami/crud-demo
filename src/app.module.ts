4c1d00eea4c42617e64fa14bc687aaa6b59de014
UTKARSH <kumar.utkarsh3396@gmail.com>
2023-05-29 08:43:11 +0530

    firstcommit

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [TypeOrmModule.forRoot({
+  type: 'sqlite',
+  database: ':memory:',
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  synchronize: true,
+  }),
+  UsersModule,
+]
+})
+export class AppModule {}
---
+++ src/users/user.entity.ts
@@ -0,0 +1,18 @@
+import { ApiProperty } from "@nestjs/swagger";
+import {Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @ApiProperty()
+    @Column()
+    firstName: string;
+
+    @ApiProperty()
+    @Column()
+    lastName: string;
+
+}
